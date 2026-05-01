71b37992479e86ea5c744d439fe79197e8daea85
PrashantPulkit <prashantpulkit5@gmail.com>
2026-05-01 13:38:00 +0530

    added config

---
+++ backend/app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = "secret"
+    JWT_SECRET_KEY = "jwt-secret"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
