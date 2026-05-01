0a026aaf76491167731625a26e0ef1e53dc6d5c2
PrashantPulkit <prashantpulkit5@gmail.com>
2026-05-01 13:52:35 +0530

    added task model

---
+++ backend/app/models/task.py
@@ -0,0 +1,7 @@
+from .. import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
