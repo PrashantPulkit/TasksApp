bbc47a94f617634bbbf74244cecb37af6b585619
PrashantPulkit <prashantpulkit5@gmail.com>
2026-05-01 13:54:37 +0530

    added util

---
+++ backend/app/utils/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask_jwt_extended import get_jwt
+from flask import jsonify
+
+def role_required(role):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            claims = get_jwt()
+            if claims.get("role") != role:
+                return jsonify({"message": "Forbidden"}), 403
+            return fn(*args, **kwargs)
+        return decorator
+    return wrapper
